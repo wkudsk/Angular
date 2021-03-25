35054ce7866bd61118d4311ed8d1a6db8988d94f
wkudsk <wkudsk@ucsc.edu>
2021-03-25 11:20:51 -0700

    Completed part 2 of the angular course

--- AngularCompleteGuide/my-first-app/src/app/server/servers/servers.component.ts
+++ AngularCompleteGuide/my-first-app/src/app/server/servers/servers.component.ts
@@ -6,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  emptyUsername = true;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  username = '';
+  detailsDisplayed = false;
+  timesPressed = [];
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+    this.checkIsUsernameEmpty();
+  }
+
+  checkIsUsernameEmpty(){
+    if(this.username === '') {
+      this.emptyUsername = true;
+      return true;
+    }
+    else {
+      this.emptyUsername = false;
+      return false;
+    }
+  }
+
+  onResetUsername() {
+    this.username = '';
+    this.checkIsUsernameEmpty();
+  }
+
+  displayParagraph() {
+    this.detailsDisplayed = !this.detailsDisplayed;
+    this.timesPressed.push(this.timesPressed.length +  1);
+  }
 }
